83b51834c17fdc39f01b6bab51ce79cdd5745415
Ovidiu <opreaovidiu_03@yahoo.com>
2020-10-02 20:27:13 +0300

    Removed unnecessary comments

--- Engine/main.cpp
+++ Engine/main.cpp
@@ -7,23 +7,3 @@ int main()
     game.run();
     return 0;
 }
-
-
-/*
-sf::Texture bgTexture;
-TextureLoader textureLoader;
-textureLoader.loadTexture(&bgTexture, "../../../Abalrose/Engine/background.png");
-sf::Sprite bgSprite;
-bgSprite.setTexture(bgTexture);
-sf::RenderWindow mWindow(sf::VideoMode(600, 400), "Abalrose");
-while(mWindow.isOpen())
-{
-  Events event;
-  //sf::Event sfEvent;
-  eventType type;
-  event.processEvents(type);
-  mWindow.clear(sf::Color(150, 150, 150, 255));
-  mWindow.draw(bgSprite);
-  mWindow.display();
-}
-*/
